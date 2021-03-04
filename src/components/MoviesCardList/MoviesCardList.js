5b6049d9f06ec13bd8c9c54230fbdcc131e08736
Aleksei Melnikov melnkv.a.a@gmail.com <melnkv.a.a@gmail.com>
2021-03-04 16:49:27 +0300

    feat: создать стили для ShowMoreButton

--- src/components/MoviesCardList/MoviesCardList.js
+++ src/components/MoviesCardList/MoviesCardList.js
@@ -2,72 +2,12 @@ import React from 'react';
 
 import MoviesCard from '../MoviesCard/MoviesCard';
 
-import MovieCardImage from '../../images/MoviesCard/movie-card-image.png';
-
 function MoviesCardList({
   locationPathname,
+  data,
 }) {
 
-  const MOVIES_CARD_LIST_DATA = [
-    {
-      id: 1,
-      title: '33 слова о дизайне',
-      subtitle: '1ч 47м',
-      imageAlt: 'кадр из фильма',
-      imageSrc: MovieCardImage,
-      isMarked: false,
-    },
-    {
-      id: 2,
-      title: '33 слова о дизайне',
-      subtitle: '1ч 47м',
-      imageAlt: 'кадр из фильма',
-      imageSrc: MovieCardImage,
-      isMarked: false,
-    },
-    {
-      id: 3,
-      title: '33 слова о дизайне',
-      subtitle: '1ч 47м',
-      imageAlt: 'кадр из фильма',
-      imageSrc: MovieCardImage,
-      isMarked: true,
-    },
-    {
-      id: 4,
-      title: '33 слова о дизайне',
-      subtitle: '1ч 47м',
-      imageAlt: 'кадр из фильма',
-      imageSrc: MovieCardImage,
-      isMarked: true,
-    },
-    {
-      id: 5,
-      title: '33 слова о дизайне',
-      subtitle: '1ч 47м',
-      imageAlt: 'кадр из фильма',
-      imageSrc: MovieCardImage,
-      isMarked: false,
-    },
-    {
-      id: 6,
-      title: '33 слова о дизайне',
-      subtitle: '1ч 47м',
-      imageAlt: 'кадр из фильма',
-      imageSrc: MovieCardImage,
-      isMarked: true,
-    },
-    {
-      id: 7,
-      title: '33 слова о дизайне',
-      subtitle: '1ч 47м',
-      imageAlt: 'кадр из фильма',
-      imageSrc: MovieCardImage,
-      isMarked: true,
-    },
-  ];
-
-  const moviesCardsMarkup = MOVIES_CARD_LIST_DATA.map((item) => (
+  const moviesCardsMarkup = data.map((item) => (
     <li
       key={item.id}
     >
--- src/components/SavedMovies/SavedMovies.js
+++ src/components/SavedMovies/SavedMovies.js
@@ -4,36 +4,51 @@ import { useLocation } from 'react-router-dom';
 
 import SearchForm from '../SearchForm/SearchForm';
 
-import Preloader from '../Preloader/Preloader';
-
 import MoviesCardList from '../MoviesCardList/MoviesCardList';
+
+import MovieCardImage from '../../images/MoviesCard/movie-card-image.png';
 
 function SavedMovies() {
 
   let location = useLocation();
 
-  const [isLoadingData, setIsLoadingData] = React.useState(true);
-
-  React.useEffect(() => {
-    const loadingDataTimeout = setTimeout(() => {
-      setIsLoadingData(false);
-    }, 1500);
-
-    return () => {
-      clearTimeout(loadingDataTimeout);
-    };
-  }, [])
+  const MOVIES_CARD_LIST_DATA = [
+    {
+      id: 1,
+      title: '33 слова о дизайне',
+      subtitle: '1ч 47м',
+      imageAlt: 'кадр из фильма',
+      imageSrc: MovieCardImage,
+      isMarked: false,
+      isShortFilm: true,
+    },
+    {
+      id: 2,
+      title: '33 слова о дизайне',
+      subtitle: '1ч 47м',
+      imageAlt: 'кадр из фильма',
+      imageSrc: MovieCardImage,
+      isMarked: false,
+      isShortFilm: false,
+    },
+    {
+      id: 5,
+      title: '33 слова о дизайне',
+      subtitle: '1ч 47м',
+      imageAlt: 'кадр из фильма',
+      imageSrc: MovieCardImage,
+      isMarked: false,
+      isShortFilm: false,
+    },
+  ];
 
   return (
     <main>
       <SearchForm />
-      {isLoadingData ? (
-        <Preloader />
-      ) : (
-        <MoviesCardList
-          locationPathname={location.pathname}
-        />
-      )}
+      <MoviesCardList
+        data={MOVIES_CARD_LIST_DATA}
+        locationPathname={location.pathname}
+      />
     </main>
   )
 }
